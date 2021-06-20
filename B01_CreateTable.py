1a56da53a20f8856fa143f69c5b5bc139e3bc171
eddiejunzhang <junzhang@aliyun.com>
2021-06-20 14:11:15 +0800

    ok

--- B01_CreateTable.py
+++ B01_CreateTable.py
@@ -19,6 +19,25 @@ def ping(ip):
         print('ping %s is ok'%ip)
         return(True)
 
+def link_postgresql_db():
+    config=configparser.ConfigParser()
+    if ping('192.168.100.20'):
+        config.read('/Users/zhangjun/Code/_privateconfig/analysis.cfg')
+    else:
+        config.read('/Users/zhangjun/Code/_privateconfig/analysis_oray.cfg')
+    # config=configparser.ConfigParser()
+    # config.read('analysis.cfg')
+    HOST = config['DB']['IP']
+    USER = config['DB']['USER']
+    DATABASE = config['DB']['DATABASE']
+    PASSWORD = config['DB']['PASSWORD']
+    PORT = config['DB']['PORT']
+    
+    print(HOST)
+    conn = psycopg2.connect(database=DATABASE, user=USER, \
+                            password=PASSWORD, host=HOST, port=PORT)
+    return conn
+
 def drop_table_tblallavailablecontrol():
     # conn = link_postgresql_db()
     
@@ -47,6 +66,37 @@ def drop_table_tblallavailablecontrol():
     conn.close()
     
     print("Droped database successfully")
+
+def create_table_tblallavailablecontrol():
+    
+    conn = link_postgresql_db()
+    
+    print("Opened database successfully")
+    
+    cur = conn.cursor()
+    
+    strSQL = '''
+    CREATE TABLE public.tblallavailablecontrol
+       (ID serial,
+       r1h         int,
+       r1t         int,
+       r2h         int,
+       r2t        int,
+       r3h        int,
+		r3t       int,
+       r4h         int,
+       r4t       int,
+       r5h       int,
+       r5t		int,
+       r6h		int,
+       r6t		int)
+    '''
+    cur.execute(strSQL)
+    print("Table created successfully")
+    
+    conn.commit()
+    conn.close()
     
 if __name__ == "__main__":
     drop_table_tblallavailablecontrol()    
+    create_table_tblallavailablecontrol()
