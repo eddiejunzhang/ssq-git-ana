ff7aaf6810a1482804ba56cc568c815e3bd51c03
Eddie Zhang <eddiezhang@markwins.com.cn>
2021-07-04 15:25:56 +0800

    modify B06

--- B06_Select_Non_Dup_into_Univ.py
+++ B06_Select_Non_Dup_into_Univ.py
@@ -5,3 +5,58 @@ B06 选择那些不重复的数据导入tblUniversalSet表中
 @author: Eddiezhang
 """
 
+import os
+import platform
+import psycopg2
+import configparser
+
+def ping(ip):
+    sys_id = platform.system()
+    if sys_id == 'Linux':
+        ret =os.system('ping -c 1 -W 1 %s'%ip) #每个ip ping 1次，等待时间为1s
+    elif sys_id == 'Windows':
+        ret =os.system('ping -w 1 %s'%ip) #每个ip ping 1次，等待时间为1s
+    elif sys_id == 'Mac':
+        ret =os.system('ping -w 1 %s'%ip) #每个ip ping 1次，等待时间为1s
+    else:
+        print('别识别到可用的操作系统。')
+    if ret:
+        print('ping %s is fail'%ip)
+        return(False)
+    else:
+        print('ping %s is ok'%ip)
+        return(True)
+
+def obtain_config_filename():
+    sys_id = platform.system()
+    
+    if sys_id == 'Linux':
+        config_filename = '/home/pi/Python_Proj/_privateconfig/analysis.cfg'
+    elif sys_id == 'Windows':
+        config_filename = 'D:\\Study\\PythonCoding\\_privateconfig\\analysis.cfg'
+    elif sys_id == 'Mac':
+        config_filename = '/Users/zhangjun/Code/_privateconfig/analysis.cfg'
+    else:
+        print('别识别到可用的操作系统。')
+    return config_filename
+    
+def link_postgresql_db():
+    config=configparser.ConfigParser()
+    if ping('192.168.100.20'):
+        config_filename = obtain_config_filename()
+        config.read(config_filename)
+    else:
+        config.read('/Users/zhangjun/Code/_privateconfig/analysis_oray.cfg')
+    # config=configparser.ConfigParser()
+    # config.read('analysis.cfg')
+    HOST = config['DB']['IP']
+    USER = config['DB']['USER']
+    DATABASE = config['DB']['DATABASE']
+    PASSWORD = config['DB']['PASSWORD']
+    PORT = config['DB']['PORT']
+    
+    print(HOST)
+    conn = psycopg2.connect(database=DATABASE, user=USER, \
+                            password=PASSWORD, host=HOST, port=PORT)
+    return conn
+
